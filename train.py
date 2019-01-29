52183f268a236505465779652af69012b40c6805
gglee <gwanggook.lee@sk.com>
2019-01-29 10:25:53 +0900

    input 3 channels -> 1 channel, draw code added

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import os
+from PIL import Image, ImageDraw
+import psutil
 
 import net
 from random import shuffle
@@ -17,15 +19,17 @@ def prepare_data_list(inpath):
             if os.path.exists(pts_path):
                 lists.append([os.path.join(inpath, l), pts_path])
 
+    assert len(lists) > 0
+
     return lists
 
 def read_data(list2train, cur_pos, batch_size=10):
-    data = np.zeros((batch_size, 48, 48, 3), dtype=np.float32)
+    data = np.zeros((batch_size, 48, 48, 1), dtype=np.float32)
     ans = np.zeros((batch_size, 68*2), dtype=np.float32)
 
     for i in range(batch_size):
         p = (i+cur_pos) % len(list2train)
-        d = np.reshape(np.fromfile(list2train[p][0], dtype=float), (48, 48, 3))
+        d = np.reshape(np.fromfile(list2train[p][0], dtype=float), (48, 48, 1))
         a = np.fromfile(list2train[p][1], dtype=float)
         data[i, :, :, :] = d / 255.0 - 1.0
         ans[i, :] = a
@@ -38,15 +42,16 @@ def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./t
 
     with slim.arg_scope(net.arg_scope()):
 
-        data_ph = tf.placeholder(tf.float32, [None, 48, 48, 3], name='input')
+        data_ph = tf.placeholder(tf.float32, [None, 48, 48, 1], name='input')
         ans_ph = tf.placeholder(tf.float32, [None, 68*2])
 
         estims, _ = net.lannet(data_ph, is_training=True)
 
         global_step = tf.Variable(0, trainable=False)
-        starter_learning_rate = 0.001
+        starter_learning_rate = 0.002
 
-        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, len(list2train)/batch_size*4, 0.96, staircase=True)
+        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, len(list2train)/batch_size*4,
+                                                   0.995, staircase=True)
         loss = tf.losses.mean_squared_error(ans_ph, estims, scope='mse')
         train_op = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss, global_step=global_step)
 
@@ -73,6 +78,25 @@ def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./t
             epoch = int(steps/num_samples)
             print('(pos %4d) Epoch %d, iter %d : loss=%f. lr=%f' % (pos, epoch, num_iter, val_loss, lr))
 
+            if epoch % 10 == 0:
+                patch = np.asarray((input_batch[0, :, :, :]+1.0)*255.0).astype('uint8').reshape((48, 48))
+                img = Image.fromarray(patch)
+                pts = np.asarray(ans_batch[0, :]).reshape((68, 2))
+
+                draw = ImageDraw.Draw(img)
+                w, h = img.width, img.height
+                for p in pts:
+                    l, t, r, b = int(p[0] * w) - 1, int(p[1] * h) - 1, int(p[0] * w) + 1, int(p[1] * h) + 1
+                    draw.ellipse((l, t, r, b))
+
+                del draw
+
+                for proc in psutil.process_iter():
+                    if proc.name() == 'display':
+                        proc.kill()
+
+                img.show()
+
         # # write save code here
         # if save_path:
         #     path_to_save = save_path
@@ -94,5 +118,5 @@ def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./t
         sess.close()
 
 if __name__ == '__main__':
-    train_list = prepare_data_list('/Volumes/Mac/Users/gglee/Data/export')
-    train(train_list, max_epoch=36)
+    train_list = prepare_data_list('/Users/gglee/Data/300W/export')
+    train(train_list, max_epoch=256)
