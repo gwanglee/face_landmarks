025617eb32c64400badc2f08bf1029232a2ee3b2
gglee <gwanggook.lee@sk.com>
2019-01-29 17:00:09 +0900

    AdamOptimizer for training

--- train.py
+++ train.py
@@ -29,7 +29,8 @@ def read_data(list2train, cur_pos, batch_size=10):
     ans = np.zeros((batch_size, 68*2), dtype=np.float32)
 
     for i in range(batch_size):
-        p = (i+cur_pos) % len(list2train)
+        cur_pos += 1
+        p = cur_pos % len(list2train)
         d = np.reshape(np.fromfile(list2train[p][0], dtype=float), (48, 48, 1))
         a = np.fromfile(list2train[p][1], dtype=float)
         data[i, :, :, :] = d / 255.0 - 1.0
@@ -37,7 +38,7 @@ def read_data(list2train, cur_pos, batch_size=10):
     return data, ans, p
 
 
-def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./train/model.ckpt'):
+def train(list2train, max_epoch=16, batch_size=32, num_threads=4, save_path='./train/model.ckpt'):
 
     num_samples = len(list2train)
 
@@ -49,13 +50,14 @@ def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./t
         estims, _ = net.lannet(data_ph, is_training=True)
 
         global_step = tf.Variable(0, trainable=False)
-        starter_learning_rate = 0.002
+        starter_learning_rate = 0.01
 
-        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, len(list2train)/batch_size*4,
-                                                   0.995, staircase=True)
-        loss = tf.losses.mean_squared_error(ans_ph, estims, scope='mse')
-        train_op = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss, global_step=global_step)
-
+        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, len(list2train), #len(list2train)/batch_size*4,
+                                                   0.998, staircase=True)
+        # loss = tf.losses.mean_squared_error(ans_ph, estims, scope='mse')
+        loss = tf.losses.huber_loss(ans_ph, estims, delta=0.01, scope='mse')
+        # train_op = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss, global_step=global_step)
+        train_op = tf.train.AdamOptimizer().minimize(loss)
         ema = tf.train.ExponentialMovingAverage(decay=0.999)
 
         init_op = tf.group(tf.global_variables_initializer(),
@@ -70,7 +72,7 @@ def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./t
 
         while epoch < max_epoch:
 
-            input_batch, ans_batch, pos = read_data(list2train, pos)
+            input_batch, ans_batch, pos = read_data(list2train, pos, batch_size=batch_size)
 
             steps, lr, val_loss, ans_pred, _ = sess.run([global_step, learning_rate, loss, estims, train_op], feed_dict={data_ph: input_batch, ans_ph: ans_batch})
             num_iter += 1
@@ -79,24 +81,24 @@ def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./t
             epoch = int(steps/num_samples)
             print('(pos %4d) Epoch %d, iter %d : loss=%f. lr=%f' % (pos, epoch, num_iter, val_loss, lr))
 
-            if epoch % 10 == 0:
-                patch = np.asarray((input_batch[0, :, :, :]+1.0)*255.0).astype('uint8').reshape((48, 48))
-                img = Image.fromarray(patch)
-                pts = np.asarray(ans_batch[0, :]).reshape((68, 2))
-
-                draw = ImageDraw.Draw(img)
-                w, h = img.width, img.height
-                for p in pts:
-                    l, t, r, b = int(p[0] * w) - 1, int(p[1] * h) - 1, int(p[0] * w) + 1, int(p[1] * h) + 1
-                    draw.ellipse((l, t, r, b))
-
-                del draw
-
-                for proc in psutil.process_iter():
-                    if proc.name() == 'display':
-                        proc.kill()
-
-                img.show()
+            # if epoch % int(max_epoch/48) == 0:
+            #     patch = np.asarray((input_batch[0, :, :, :]+1.0)*255.0).astype('uint8').reshape((48, 48))
+            #     img = Image.fromarray(patch)
+            #     pts = np.asarray(ans_batch[0, :]).reshape((68, 2))
+            #
+            #     draw = ImageDraw.Draw(img)
+            #     w, h = img.width, img.height
+            #     for p in pts:
+            #         l, t, r, b = int(p[0] * w) - 1, int(p[1] * h) - 1, int(p[0] * w) + 1, int(p[1] * h) + 1
+            #         draw.ellipse((l, t, r, b))
+            #
+            #     del draw
+            #
+            #     for proc in psutil.process_iter():
+            #         if proc.name() == 'display':
+            #             proc.kill()
+            #
+            #     img.show()
 
         # write save code here
         if save_path:
@@ -115,4 +117,4 @@ def train(list2train, max_epoch=16, batch_size=64, num_threads=4, save_path='./t
 
 if __name__ == '__main__':
     train_list = prepare_data_list('/Users/gglee/Data/300W/export')
-    train(train_list, max_epoch=256, save_path='/Users/gglee/Data/300W/model')
+    train(train_list, max_epoch=999, save_path='/Users/gglee/Data/300W/model')
