818bbebd1477380328a6ea9bf8bbcad599574f79
영상인식기술 Cell 이광국 <gwanggook.lee@sk.com>
2019-03-26 12:10:57 +0900

    indent fix

--- train_slim.py
+++ train_slim.py
@@ -146,12 +146,12 @@ if __name__=='__main__':
         if FLAGS.regularizer:
             regularizer = _config_weights_regularizer(FLAGS.regularizer, 0.001)
 
-    # predictions, _ = net.lannet(image, is_training=True)
-    with tf.variable_scope('model') as scope:
-        intensor = tf.identity(image, 'input')
-        predictions, _ = net.lannet(intensor, is_training=True, normalizer_fn=norm_fn, normalizer_params=norm_params,
-                                    regularizer=regularizer, depth_mul=FLAGS.depth_multiplier)
-        # val_pred, _ = net.lannet(val_imgs, is_training=False)
+        # predictions, _ = net.lannet(image, is_training=True)
+        with tf.variable_scope('model') as scope:
+            intensor = tf.identity(image, 'input')
+            predictions, _ = net.lannet(intensor, is_training=True, normalizer_fn=norm_fn, normalizer_params=norm_params,
+                                        regularizer=regularizer, depth_mul=FLAGS.depth_multiplier)
+            # val_pred, _ = net.lannet(val_imgs, is_training=False)
 
         loss = slim.losses.absolute_difference(points, predictions)
         total_loss = slim.losses.get_total_loss()
