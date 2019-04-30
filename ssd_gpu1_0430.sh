d54ee8629227fd3c84b4ac8030588abddf29f686
영상인식기술 Cell 이광국 <gwanggook.lee@sk.com>
2019-04-30 14:48:57 +0900

    exp scripts updated

--- ssd_gpu1_0430.sh
+++ ssd_gpu1_0430.sh
@@ -1,7 +1,14 @@
-# gpu=0
-# export CUDA_VISIBLE_DEVICES=$gpu
 CONFIG=ssd_face_128_v4.config
-TRAIN=ssd_face_160_v4
+TRAIN=ssd_face_128_v4
+export PYTHONPATH=/youjin/face_landmark/models_latest/research:/youjin/face_landmark/models_latest/research/slim
+
+python ../models_latest/research/object_detection/model_main.py --logtostderr \
+--pipeline_config_path=./$CONFIG \
+--model_dir=../train/$TRAIN
+
+
+CONFIG=ssd_face_128_v5.config
+TRAIN=ssd_face_128_v5
 export PYTHONPATH=/youjin/face_landmark/models_latest/research:/youjin/face_landmark/models_latest/research/slim
 
 python ../models_latest/research/object_detection/model_main.py --logtostderr \
--- ssd_gpu2_0430.sh
+++ ssd_gpu2_0430.sh
@@ -1,9 +1,16 @@
-# gpu=0
-# export CUDA_VISIBLE_DEVICES=$gpu
-CONFIG=ssd_face_144_v1.config
-TRAIN=ssd_face_144_v1
-export PYTHONPATH=/youjin/face_detection/Program/models_latest/research:/youjin/face_detection/Program/models_latest/research/slim
+CONFIG=ssd_face_128_v6.config
+TRAIN=ssd_face_128_v6
+export PYTHONPATH=/youjin/face_landmark/models_latest/research:/youjin/face_landmark/models_latest/research/slim
 
-python ../../face_detection/Program/models_latest/research/object_detection/model_main.py --logtostderr \
+python ../models_latest/research/object_detection/model_main.py --logtostderr \
+--pipeline_config_path=./$CONFIG \
+--model_dir=../train/$TRAIN
+
+
+CONFIG=ssd_face_128_v7.config
+TRAIN=ssd_face_128_v7
+export PYTHONPATH=/youjin/face_landmark/models_latest/research:/youjin/face_landmark/models_latest/research/slim
+
+python ../models_latest/research/object_detection/model_main.py --logtostderr \
 --pipeline_config_path=./$CONFIG \
 --model_dir=../train/$TRAIN
