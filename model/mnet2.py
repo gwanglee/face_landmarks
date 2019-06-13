17e1fe8a03723a6019f8ddc4ed1514ef613a733d
영상인식기술 Cell 이광국 <gwanggook.lee@sk.com>
2019-06-13 09:25:00 +0900

    mobilenet: feature_map + own fc layer

--- model/mnet2.py
+++ model/mnet2.py
@@ -12,17 +12,16 @@ sys.path.append('/Users/gglee/Develop/models/research/slim/nets')
 from mobilenet import mobilenet_v2
 
 def mnet2(input_tensor, activation_fn=tf.nn.relu6, depth_multiplier=0.35):
-    with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope()):
-        logits, endpoints = mobilenet_v2.mobilenet(input_tensor, num_classes=68*2, depth_multiplier=depth_multiplier,
-                                                   activation_fn=activation_fn)
-        return logits, endpoints
+    end_points = {}
 
-        # num_classes = 1001,
-        # depth_multiplier = 1.0,
-        # scope = 'MobilenetV2',
-        # conv_defs = None,
-        # finegrain_classification_mode = False,
-        # min_depth = None,
-        # divisible_by = None,
-        # activation_fn = None,
-        # ** kwargs
+    with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope()):
+        feature_map = end_points['ftr_map'] = mobilenet_v2.mobilenet_base(input_tensor, depth_multiplier=depth_multiplier)
+
+        net = slim.flatten(feature_map)
+        net = slim.fully_connected(net, (256 * depth_multiplier), scope='fc6')  # none -> fc6
+        net = slim.fully_connected(net, 68 * 2, scope='fc7')
+
+    return net, end_points
+        # logits, endpoints = mobilenet_v2.mobilenet(input_tensor, num_classes=68*2, depth_multiplier=depth_multiplier,
+        #                                            activation_fn=activation_fn)
+        # return logits, endpoints
