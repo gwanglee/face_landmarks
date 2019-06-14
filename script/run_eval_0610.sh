c7ee10dfb2e4894ac76fbbaa12918bc3ea0e3116
영상인식기술 Cell 이광국 <gwanggook.lee@sk.com>
2019-06-14 10:01:21 +0900

    added eval models

--- script/run_eval_0610.sh
+++ script/run_eval_0610.sh
@@ -1,41 +1,71 @@
 #!/usr/bin/env bash
-NAME='ssd_face_128_v21'
+#NAME='ssd_face_128_v21'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_128_v22'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_128_v24'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_128_v25'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_160_v23'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_160_v26'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_160_v29'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+
+#NAME='ssd_face_128_v28'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_128_v30'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+#
+#NAME='ssd_face_144_v27'
+#python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
+#                                --images_dir=/Users/gglee/Data/face_eval/data \
+#                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
+#                                --disp=True
+
+NAME='ssd_face_128_v0'
 python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
                                 --images_dir=/Users/gglee/Data/face_eval/data \
                                 --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
                                 --disp=True
 
-NAME='ssd_face_128_v22'
-python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
-                                --images_dir=/Users/gglee/Data/face_eval/data \
-                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
-                                --disp=True
-
-NAME='ssd_face_128_v24'
-python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
-                                --images_dir=/Users/gglee/Data/face_eval/data \
-                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
-                                --disp=True
-
-NAME='ssd_face_128_v25'
-python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
-                                --images_dir=/Users/gglee/Data/face_eval/data \
-                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
-                                --disp=True
-
-NAME='ssd_face_160_v23'
-python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
-                                --images_dir=/Users/gglee/Data/face_eval/data \
-                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
-                                --disp=True
-
-NAME='ssd_face_160_v26'
-python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
-                                --images_dir=/Users/gglee/Data/face_eval/data \
-                                --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
-                                --disp=True
-
-NAME='ssd_face_160_v29'
+NAME='ssd_face_300_v0'
 python util/detect_face_landmark.py --face_checkpoint_dir=/Users/gglee/Data/TFModels/0610/$NAME/freeze \
                                 --images_dir=/Users/gglee/Data/face_eval/data \
                                 --write_txt_dir=/Users/gglee/Data/face_eval/det/$NAME \
